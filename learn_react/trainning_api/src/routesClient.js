c08a11f1fb5beaa98cce9f700648139c639bb922
NguyenThiThuc97 <ndtt8797@gmail.com>
2018-12-21 12:00:58 +0700

    commit

---
+++ learn_react/trainning_api/src/routesClient.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const RoutesClient = [
+    // {
+    //     path : "/home",
+    //     exact : true,
+    //     main : () => return 
+    // }
+]
+
+export default RoutesClient;
