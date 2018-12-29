389b7219b328abcb34411b18080ec2ca52baecd7
NguyenThiThuc97 <ndtt8797@gmail.com>
2018-12-29 20:31:17 +0700

    commit

--- learn_react/trainning_api/src/pages/homePage/homePage.js
+++ learn_react/trainning_api/src/pages/homePage/homePage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class HomePage extends Component {
   render() {
     return (
-        <img className = "img-fluid image-home-admin pdt-45" src = "http://goodsamaritanfs.com/wp-content/uploads/2014/03/Admin.-Staff.png"/>
+        <img className = "img-fluid image-home-admin pdt-45" src = "http://www.pandeycoachingvns.com/wp-content/uploads/2017/07/Staff-Banner.png"/>
       );
   }
 }
